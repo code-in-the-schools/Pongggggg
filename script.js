7c6950beabad85076227b4065d0eae6186fb1560
Morris Williams <morriswilliams4035@gmail.com>
2020-08-03 15:42:11 +0000

    I changed the size of my canvas and the size of the ball. All attempts to get the ball and the paddles to collide were unsuccessful.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-var canvasWidth = 500, canvasHeight = 400;
-var ballSize=20 // make ball
+var canvasWidth = 500, canvasHeight = 425;
+var ballSize=15 // make ball
 var myBall;
 var myBall_xPos = canvasWidth/2, myBall_yPos = canvasHeight/2;
-var myBall_xVel = 0, myBall_yVel = 0;
-var myBall_top    = myBall_yPos - ballSize/2,
-
-
+var myBall_xVel = 5, myBall_yVel = 0;
+var myBall_top    = myBall_yPos - ballSize/2;
+var paddle_left, paddle_right;
+var paddleWidth = 10, paddleHeight  = canvasHeight/5, paddleVel = 5,
+	paddleLeft_xPos = ballSize, paddleRight_xPos = canvasWidth- ballSize,
+ 	 paddleLeft_yPos = canvasHeight/2, paddleRight_yPos = canvasHeight/ 2;
+	paddleLeft_top    = paddleLeft_yPos - paddleHeight/2,
+	paddleLeft_bottom = paddleLeft_yPos + paddleHeight/2,
+	paddleRight_top    = paddleRight_yPos - paddleHeight/2,
+	paddleRight_bottom = paddleRight_yPos + paddleHeight/2;
 
 	myBall_bottom = myBall_yPos + ballSize/2,
 	myBall_left   = myBall_xPos - ballSize/2,
@@ -18,13 +24,17 @@ function setup() {
 	myBall = rect(myBall_xPos,myBall_yPos,ballSize,ballSize);
 	myBall_xVel = random(3,6);
 	myBall_yVel = random(2,4);
-}// motion
+// motion
+ paddle_left=rect(50,50,paddleWidth,paddleHeight)
+}
 
   function draw() {
 	background(color(r, g, b));
-
-	moveAndBounceWall();
-
+rect(paddleLeft_xPos,paddleLeft_yPos, paddleWidth, paddleHeight);
+	rect(paddleRight_xPos,paddleRight_yPos, paddleWidth, paddleHeight);
+	// make game paddles 
+  moveAndBounceWall();
+movePaddles();
 	rect(myBall_xPos,myBall_yPos,ballSize,ballSize);
 	}
 // ball bouncing
@@ -48,15 +58,37 @@ function moveAndBounceWall() {
   	myBall_yVel = -myBall_yVel;
 		colorChange();
 	}
+}               
+    function movePaddles(){
+      	paddleLeft_top    = paddleLeft_yPos - paddleHeight/2;
+	paddleLeft_bottom = paddleLeft_yPos + paddleHeight/2;
+	paddleRight_top    = paddleRight_yPos - paddleHeight/2;
+	paddleRight_bottom = paddleRight_yPos + paddleHeight/2;
+
+if (keyIsDown(87) && (paddleLeft_top > 0)) {
+		paddleLeft_yPos -= paddleVel;
+	} else if (keyIsDown(83) && (paddleLeft_bottom < canvasHeight)) {
+		paddleLeft_yPos += paddleVel;
+	}
+  if (keyIsDown(38) && (paddleRight_top > 0)) {
+		paddleRight_yPos -= paddleVel;
+	} else if (keyIsDown(40) && (paddleRight_bottom < canvasHeight)) {
+		paddleRight_yPos += paddleVel;
+	}
 }
 
 function colorChange() {
-	r = random(255);
-	  g = random(255);
-	 b = random(255);
+	r = random(300);
+	  g = random(300);
+	 b = random(330);
 }
 
-// make game paddles
+//function collision() {
+
+//}
+
+
 // keep the score
 // reset ball in middle
 // Mission Complete!
+// (Extra) Sound Effects
